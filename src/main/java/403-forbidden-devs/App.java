064d155fc05946dd6edefafb0d7ec1fc4f628e94
Himanshu Pandey <himanshu.pandey@piramal.com>
2024-01-13 17:04:55 +0530

    hp77 | add skeleton for mvn project

---
+++ src/main/java/403-forbidden-devs/App.java
@@ -0,0 +1,13 @@
+package 403-forbidden-devs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
